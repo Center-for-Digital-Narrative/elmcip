72b5837bdbd00f344bb9e038ca93dfce3a59e6f3
elisabeth <elisabeth@mail.multiplum.com>
2013-01-08 14:14:42 +0100

    Returned the search form values to 15

--- template.php
+++ template.php
@@ -165,7 +165,7 @@ function elmcip_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
 //    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
     $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
-    $form['search_block_form']['#size'] = 30;  // define size of the textfield
+    $form['search_block_form']['#size'] = 15;  // define size of the textfield
 //    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
 //    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
     $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
