cff613045d5f06092e96c241fc649628f9670eba
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2013-09-15 22:59:44 +0200

    Reformatting and adding missing docblock.

--- template.php
+++ template.php
@@ -141,20 +141,19 @@ function localhost_smb_ev_preprocess_block(&$variables, $hook) {
 }
 */
 
+/**
+ * Implement hook_form_alter.
+ */
 function elmcip_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
-//    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
     $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
     $form['search_block_form']['#size'] = 25;  // define size of the textfield
-//    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
-//    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
     $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
 
-// Add extra attributes to the text box
+    // Add extra attributes to the text box
     $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
     $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
     // Prevent user from searching the default text
     $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
   }
-} 
-
+}
