16d62c82ff06c699aba0cb9668f042217947200d
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2016-11-02 18:05:05 +0100

    Issue 39 Take over field control.
    - Field standard markup.

--- themes/elmcip/template.php
+++ themes/elmcip/template.php
@@ -176,3 +176,28 @@ function elmcip_panels_default_style_render_region($variables) {
   $output .= implode('', $variables['panes']);
   return $output;
 }
+
+/**
+ * implement theme_field().
+ */
+function elmcip_field__field_pullquote($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
