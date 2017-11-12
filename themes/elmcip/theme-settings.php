a9c6e63b553ae17b7466f91bc313eafd583008bb
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2017-11-12 16:37:05 +0100

    Issue 300 Updated theme name

--- themes/elmcip/theme-settings.php
+++ themes/elmcip/theme-settings.php
@@ -7,7 +7,7 @@
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+function elmcip_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
