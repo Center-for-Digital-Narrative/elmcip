474a3f8df3ed8062efddabbb2e6f6ec4f7a31fd9
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2018-10-09 18:43:18 +0200

    Issue 390 Fix theme template files
    Re-add theme template files. Temp. removed tracking down
    extra jquery submit buttong in media browser modal.

---
+++ themes/elmcip/templates/ds-2col.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column template.
+ */
+?>
+<div class="<?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php if ($left): ?>
+    <div class="group-left<?php print $left_classes; ?>">
+      <div class="inner">
+        <?php print $left; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div class="group-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </div>
+  <?php endif; ?>
+
+</div>
