da4b01440f5dce338b10d5424e978119537e19bf
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-09-26 19:21:56 +0200

    Do not hard code master branch
    Return default git online format.

--- bin/branch_changed.sh
+++ bin/branch_changed.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-git checkout master || exit
 original_head=$(git rev-parse HEAD) || exit
 git pull origin || exit
 updated_head=$(git rev-parse HEAD) || exit
 
 if test "$updated_head" = "$original_head"; then
   echo Upstream is idling
+  exit 1
 else
   echo These new commits were brought in
-  git shortlog "$original_head".."$updated_head"
+  git log "$original_head".."$updated_head" --oneline
+  exit 0
 fi
