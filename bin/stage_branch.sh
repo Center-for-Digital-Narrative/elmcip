f835a5d0aa08b5b1d42941d01121bbe565d53d65
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-10-02 11:36:06 +0200

    Staging script - Teardown should not check repo for changes
    Teardown should always happen when asked for.

--- bin/stage_branch.sh
+++ bin/stage_branch.sh
@@ -13,55 +13,55 @@ then
     exit 1
 fi
 
-date
-bin/branch_changed.sh
+drush --version || exit 1
 
-if [ $? -eq 0 ]
+if [ "$1" = "reset" ]
 then
-    drush --version || exit
+    date
+    echo "Tear down site and upgrade from database snapshot."
 
-    if [ $1 = "reset" ]
+    # Test if $DB_DIR exists
+    if [ ! -d $DB_DIR ]
     then
-        echo "Tear down site and upgrade from database snapshot"
-
-        # Test if $DB_DIR exists
-        if [ ! -d $DB_DIR ]
-        then
-            echo "Error: Database snapshot directory: $DB_DIR does not exists"
-            exit 1
-        fi
-
-        ## Create db. snapshot
-        cd $DRUPAL || exit
-        drush sql-dump --result-file="${DB_DIR}/latest.elmcip.sql" --gzip || exit
-
-        ## Empty database and re-populate from production snapshot
-        if [ ! -f "${DB_DIR}/elmcip/latest.elmcip.sql" ]
-        then
-            echo "Error: Database snapshot: $DB_DIR/elmcip/latest.elmcip.sql do not exists"
-            exit 1
-        fi
-
-        drush sql-drop || exit
-        gunzip -c /elmcip/latest.elmcip.sql | /elmcip/applications/elmcip.net/vendor/drush/drush/drush sql-cli
-
-        ## Enable us to load file and images on demand into staging area from prod.
-        drush pm-enable --yes stage_file_proxy
-        drush variable-set stage_file_proxy_origin "https://elmcip.net"
-
-        ## Password protect site. Stop content from getting picked up by spider bots.
-        cat /elmcip/applications/htaccess.txt >> .htaccess
-        cd $HOMEDIR || exit
-
-    elif [ $1 = "normal" ]
-    then
-        echo "Upgrading site to latest development version"
-        git submodule foreach git reset --hard || exit
-        bin/site-upgrade master
-    else
-        echo echo "Error: Valid parameter: stage_branch normal|reset"
+        echo "Error: Database snapshot directory: $DB_DIR does not exists"
         exit 1
     fi
+
+    ## Create db. snapshot
+    cd $DRUPAL || exit
+    drush sql-dump --result-file="${DB_DIR}/latest.elmcip.sql" --gzip || exit
+
+    ## Empty database and re-populate from production snapshot
+    if [ ! -f "${DB_DIR}/elmcip/latest.elmcip.sql" ]
+    then
+        echo "Error: Database snapshot: $DB_DIR/elmcip/latest.elmcip.sql do not exists"
+        exit 1
+    fi
+
+    drush sql-drop || exit
+    gunzip -c /elmcip/latest.elmcip.sql | /elmcip/applications/elmcip.net/vendor/drush/drush/drush sql-cli
+
+    ## Enable us to load file and images on demand into staging area from prod.
+    drush pm-enable --yes stage_file_proxy
+    drush variable-set stage_file_proxy_origin "https://elmcip.net"
+
+    ## Password protect site. Stop content from getting picked up by spider bots.
+    cat /elmcip/applications/htaccess.txt >> .htaccess
+    cd "$HOMEDIR" || exit 1
+
+elif [ "$1" = "normal" ]
+then
+    if ! bin/branch_changed.sh
+    then
+      exit 1
+    fi
+
+    date
+    echo "Upgrading site to latest development version."
+
+    git submodule foreach git reset --hard || exit 1
+    bin/site-upgrade master
 else
-    echo "Nothing to do"
+    echo echo "Error: Valid parameter: stage_branch normal|reset"
+    exit 1
 fi
