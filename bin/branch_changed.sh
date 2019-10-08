26872419312ede8d9549841887cea5f0b534da5f
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-10-08 17:06:50 +0200

    Reduce staging log entries

--- bin/branch_changed.sh
+++ bin/branch_changed.sh
@@ -5,7 +5,6 @@ git pull origin || exit 1
 updated_head=$(git rev-parse HEAD) || exit 1
 
 if test "$updated_head" = "$original_head"; then
-  echo Upstream is idling
   exit 1
 else
   echo These new commits were brought in
