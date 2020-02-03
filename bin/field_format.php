0657647393827912c21581b3d3155263be4cb40d
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-02-03 23:03:23 +0100

    Updated field text format utility

--- bin/field_format.php
+++ bin/field_format.php
@@ -6,6 +6,7 @@ final class Process
   private $contentType;
   private $fields;
   private $goodFormats;
+  private $entity;
 
   public function __construct(string $contentType, array $fields, array $goodFormats)
   {
@@ -32,33 +33,53 @@ final class Process
     $news_items_nids = array_keys($result['node']);
 
     foreach ($news_items_nids as $nid) {
-      $entity = entity_metadata_wrapper('node', $nid);
-      $this->field($entity);
+      $this->entity = entity_metadata_wrapper('node', $nid);
+      $this->field();
     }
   }
 
-  private function field($entity)
+  private function field(): void
   {
     foreach ($this->fields as $field) {
       $name = $field['field_name'];
-      $info = $entity->getPropertyInfo();
+      $info = $this->entity->getPropertyInfo();
 
       if ($info[$name]['type'] === 'text_formatted') {
-        $this->validateFormat($entity->$name->raw(), $field);
+        $illegalField = $this->validateFormat($field);
+        $this->alterFormat($illegalField, 'basic_text_editor');
       }
     }
   }
 
-  private function validateFormat($raw, array $field): void {
+  private function validateFormat(array $field): string
+  {
+    $field_name = $field['field_name'];
+    $raw = $this->entity->$field_name->value();
+
     if (!$raw) {
+      return '';
+    }
+
+    $format = $this->entity->$field_name->format->value();
+
+    if (!in_array($format, $this->goodFormats, FALSE)) {
+      print "Illegal format. Node: {$this->entity->getIdentifier()} {$field_name} Format: {$format} \n";
+      return $field_name;
+    }
+
+    return '';
+  }
+
+  private function alterFormat($field, $format): void
+  {
+    if (!$field) {
       return;
     }
 
-    if (!in_array($raw['format'], $this->goodFormats, FALSE)) {
-      print $field['field_name'] . PHP_EOL;
-      print $raw['format'] . PHP_EOL;
-    }
+    $this->entity->$field->format = $format;
+    $this->entity->save();
   }
+
 }
 
 $entityType = 'node';
