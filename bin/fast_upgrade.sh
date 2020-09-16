ea197f58658e0949320d2a1071dea8435eaef865
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-09-16 13:13:01 +0200

    Exit if drupal web root is not found.

--- bin/fast_upgrade.sh
+++ bin/fast_upgrade.sh
@@ -6,13 +6,13 @@ DRUPAL=drupal
 URI=elmcip.local
 HOMEDIR=${PWD}
 
-cd ${HOMEDIR}/${DRUPAL}
+cd ${HOMEDIR}/${DRUPAL} || exit
 drush sql-drop --yes
 gunzip -c ../site/latest.elmcip.sql.gz | drush sql-cli
 
 drush updatedb --yes
 drush features-revert-all --yes
 drush fl
-drush uli -l elmcip.local
+drush uli -l $URI
 drush en --yes development_settings
 drush cc all
