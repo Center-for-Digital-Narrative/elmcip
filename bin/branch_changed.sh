c52bbb46d3f62d19bde5b89f13912f6f9295682d
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-09-16 13:12:07 +0200

    NIRD test staging always timestamp
    Staging log new also contain timestamp. Will make checkin when
    last quick upgrade did run, debugging non running cron tasks.

--- bin/branch_changed.sh
+++ bin/branch_changed.sh
@@ -5,6 +5,7 @@ git pull origin || exit 1
 updated_head=$(git rev-parse HEAD) || exit 1
 
 if test "$updated_head" = "$original_head"; then
+  date +'%Y-%m-%d %H:%M'
   exit 1
 else
   echo These new commits were brought in
