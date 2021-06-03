0e60c92e4207476d7813d4eae0d70b9a4b349cf4
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-06-03 13:19:40 +0200

    Move tabs above title and use flexbox for layout
    - Clean up old layout bethod for tabs.
    - Position tab above node title is where users
    these days expect it to be positioned.
    - Make buttons easier to spot by giving them a
    default background and hover color.

--- themes/elmcip/templates/page.tpl.php
+++ themes/elmcip/templates/page.tpl.php
@@ -118,17 +118,19 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if ($tabs = render($tabs)): ?>
-        <div class="tabs"><?php print $tabs; ?></div>
+        <div class="tabs tabs-wrapper"><?php print $tabs; ?></div>
       <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
+
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+
+        <?php print render($page['help']); ?>
+<?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
