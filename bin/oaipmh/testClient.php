c205c447e45552f465756073db867c639b5b93a6
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-08-24 09:34:02 +0200

    Running local tests

--- bin/oaipmh/testClient.php
+++ bin/oaipmh/testClient.php
@@ -5,30 +5,41 @@ use Phpoaipmh\Endpoint;
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-$client = new Client('http://elmcip.local/data/work/oai-pmh');
+$client = new Client('http://elmcip-d9.local/oai/request');
 try {
   $myEndpoint = new Endpoint($client);
 } catch (Exception $e) {
 }
 
-$iterator = $myEndpoint->listRecords('oai_dc');
-echo "Dublin Core - Total count is " . ($iterator->getTotalRecordCount() . PHP_EOL ?: 'unknown');
+//$iterator = $myEndpoint->listRecords('oai_dc');
+//echo "Dublin Core - Total count is " . ($iterator->getTotalRecordCount() . PHP_EOL ?: 'unknown');
 
-$iterator = $myEndpoint->listRecords('mods');
-echo "MODS - Total count is " . ($iterator->getTotalRecordCount() . PHP_EOL ?: 'unknown');
+//$iterator = $myEndpoint->listRecords('oai_raw');
+//echo "MODS - Total count is " . ($iterator->getTotalRecordCount() . PHP_EOL ?: 'unknown');
 
-$result = $myEndpoint->identify();
-var_dump($result);
+//$result = $myEndpoint->identify();
+//var_dump($result);
 
+$resultSet = [];
 echo "End point formats" . PHP_EOL;
 $results = $myEndpoint->listMetadataFormats();
 foreach($results as $item) {
-  print_r($item);
+  $format = (string) $item->metadataPrefix;
+  $iterator = $myEndpoint->listRecords($format);
+  echo 'Format: ' . $format . ' Total count is: ' . ($iterator->getTotalRecordCount() . PHP_EOL ?: 'unknown');
+
+
+  foreach ($iterator as $record) {
+    var_dump($record);
+//  $id = $record->metadata->$format->identifier;
+//    $resultSet[$format][] = $record;
+  }
+
 }
 
-$creativeWorks = $myEndpoint->listRecords('mods');
-$resultSet = [];
-foreach ($creativeWorks as $creativeWork) {
-  $resultSet[] = $creativeWork->metadata->mods->identifier;
-  echo $creativeWork->metadata->mods->identifier . PHP_EOL;
-}
+//$creativeWorks = $myEndpoint->listRecords('oai_dc');
+//$resultSet = [];
+//foreach ($creativeWorks as $creativeWork) {
+//  $resultSet[] = $creativeWork->metadata->oai_dc->identifier;
+//  echo $creativeWork->metadata->oai_dc->identifier . PHP_EOL;
+//}
